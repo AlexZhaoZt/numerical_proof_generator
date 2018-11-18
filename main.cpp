2db29db1806197ef3ca3eda157e3d22092114994
AlexZhaoZt <zhaoztalex@hotmail.com>
2018-11-18 14:14:05 -0600

    Implemented weak numerical proof. Also added a new type of proof, summation proof, that takes the sum of the traget number and performs regular numerical proof on it.

--- main.cpp
+++ main.cpp
@@ -1,72 +1,117 @@
-#include <iostream>
-#include <string>
-#include <stack>
-#include <array>
-#include <queue>
-#include <cmath>
-
-#include <vector>
-
 
 #include "main.h"
-using std::pow;
-using std::cout;
-using std::cin;
-using std::endl;
-using std::string;
-using std::stack;
 
-int counter = 0;
+
+
 double desired;
-int length_;
+
+vector<double> base;
+
+/* Maintains the number of ways to decompose the given number. */
+int counter = 0;
+
+/* Maintains the number of calculations performed. */
+int calculation = 0;
 
 int main() {
-  using std::vector;
   cout << "Enter a number for 114514 decomposition:" << endl;
   cin >> desired;
-//Attempt Strong Numerical Proof.
-//尝试强算术论证
-  length_ = 11;
-  int array[length_]; //= new int[length_];
-  array[0] = 0;
-  array[1] = 0;
-  put(array, 2, 2, 0);
-  int newArr[length_];
-  newArr[0] = -1;
-  newArr[1] = 0;
-  put(newArr, 2, 2, 0);
-  if (counter >= 1) {
-    cout << "Found " + std::to_string(counter) + " solutions." << endl;
-  } else {
-    cout << "Found " + std::to_string(counter) + " solution." << endl;
+  //Attempt Strong Numerical Proof.
+  //尝试强算术论证
+  // int array[length_]; //= new int[length_];
+  strongProof();
+
+  string answer;
+  cout << "Do you wish to continue with weak numerical proof? (Y/N)" << endl;
+  cin >> answer;
+
+  if (answer == "N") {
+    cout << "Quitting..." << endl;
+    return 0;
   }
+
+
 //Attempt Weak Numerical Proof.
 //尝试弱算术论证
 //incomplete
-/*
-  vector<string> v;
-  string put_in = "";
-  char charArr[6] = {'1', '1', '4', '5', '1', '4'};
-  for (int block = 2; block < 6; block++) {
-    for (int chance = 0; chance < charArr.size() - (block - 1); chance++) {
-      int used = 0;
-      for (size_t index = 0; index < charArr.size() - (block - 1); index++) {
-        if (used < chance) {
-          for (int i = 0; i < block; i++) {
-            put_in += charArr[index + i];
-          }
-          v.push_back(put_in);
-          used++;
-          put_in = "";
-        } else {
-          put_in += charArr[index];
-          v.push_back(put_in);
-          put_in = "";
-        }
-      }
+  weakProof();
+
+  answer = "";
+  cout << "Do you wish to continue with summation proof? (Y/N)" << endl;
+  cin >> answer;
+
+  if (answer == "N") {
+    cout << "Quitting..." << endl;
+    return 0;
+  }
+
+  double sum = 0.0;
+  string number = to_string(desired);
+  int notZero = 0;
+  for (int i = 0; i < (int) number.size(); i++) {
+    if (number.at(i) != '0') {
+      notZero = i;
     }
   }
-  */
+  number = number.substr(0, notZero);
+  for (size_t i = 0; i < number.size(); i++) {
+    sum += (double) (number.at(i) - '0');
+    if (i == number.size() - 1) {
+      cout << number.at(i);
+    } else {
+      cout << number.at(i) << "+";
+    }
+  }
+  cout << "=" << sum << endl;
+  desired = sum;
+  strongProof();
+  if (counter == 0) {
+    cout << "Proceeding with weak numerical proof..." << endl;
+    weakProof();
+  }
+
+
+
+
+
+  answer = "";
+  cout << "Do you wish to continue with Po Zhen proof? (Y/N)" << endl;
+  cin >> answer;
+
+  if (answer == "N") {
+    cout << "Quitting..." << endl;
+    return 0;
+  }
+
+  int i = 0;
+  for (char ch : number) {
+    string output;
+    switch (ch) {
+      case '0': output = string(1, ch) + "=1-1^(4+5+1+4)";
+                break;
+      case '1': output = string(1, ch) + "=1^(1+4+5+1+4)";
+                break;
+      case '2': output = string(1, ch) + "=1+1^(4-5-1+4)";
+                break;
+      case '3': output = string(1, ch) + "=1-1+(4-5)^1+4";
+                break;
+      case '4': output = string(1, ch) + "=1*1*4^(5*1-4)";
+                break;
+      case '5': output = string(1, ch) + "=1-(1+(4-5)^1)+4";
+                break;
+      case '6': output = string(1, ch) + "=1*1-(4-5*1)+4";
+                break;
+      case '7': output = string(1, ch) + "=1+1^(4+5)+1+4";
+                break;
+      case '8': output = string(1, ch) + "=(1*1-(4-5)*1)*4";
+                break;
+      case '9': output = string(1, ch) + "=(1-1+4+5)*(1^4)";
+                break;
+    }
+
+    cout << output << endl;
+    i++;
+  }
 
 
 
@@ -74,30 +119,129 @@ int main() {
 
 }
 
+void strongProof() {
+  calculation = 0;
+  counter = 0;
+  base.clear();
+  base.push_back(1.0);
+  base.push_back(1.0);
+  base.push_back(4.0);
+  base.push_back(5.0);
+  base.push_back(1.0);
+  base.push_back(4.0);
+  vector<int> array;
+  array.push_back(0);
+  array.push_back(0);
+  // array[0] = 0;
+  // array[1] = 0;
+
+  put(array, 11);
+
+  array.clear();
+  array.push_back(-1);
+  array.push_back(0);
+  // int newArr[length_];
+  // newArr[0] = -1;
+  // newArr[1] = 0;
+  put(array, 11);
+  if (counter >= 1) {
+    cout << "Strong numerical proof found " + std::to_string(counter) + " solutions." << endl;
+  } else {
+    cout << "Strong numerical proof found " + std::to_string(counter) + " solution." << endl;
+  }
+  cout << "Performed " << calculation << " calculations." << endl;
+}
+
+void weakProof() {
+  calculation = 0;
+  counter = 0;
+  queue<char> q;
+  q.push('1');
+  q.push('1');
+  q.push('4');
+  q.push('5');
+  q.push('1');
+  q.push('4');
+  vector<double> v;
+  choose(q, v);
+  // string put_in = "";
+  // char charArr[6] = {'1', '1', '4', '5', '1', '4'};
+  // for (int firstSize = 1; firstSize < 6; firstSize++) {
+  //
+  //
+  //
+  // }
+  if (counter >= 1) {
+    cout << "Weak numerical proof found " + std::to_string(counter) + " solutions." << endl;
+  } else {
+    cout << "Weak numerical proof found " + std::to_string(counter) + " solution." << endl;
+  }
+  cout << "Performed " << calculation << " calculations." << endl;
+}
+
+void choose(queue<char> remain, vector<double> current) {
+  string input = "";
+  if (remain.empty()) {
+    base = current;
+    int max_length = current.size() * 2 - 1;
+    if (max_length > 2 && max_length < 11) {
+      vector<int> array;
+      array.push_back(0);
+      array.push_back(0);
+      put(array, max_length);
+      array.clear();
+      array.push_back(-1);
+      array.push_back(0);
+      put(array, max_length);
+    }
+    return;
+  }
+  while (!remain.empty()) {
+    input += remain.front();
+    remain.pop();
+    current.push_back((double) stoi(input));
+    // used++;
+
+    choose(remain, current);
+    current.pop_back();
+  }
+}
+
+void put(vector<int> v, int max_length) {
+  put(v, 2, 2, 0, max_length);
+}
+
 //recursively put in numbers and symbols in Reverse Polish Notation (RPN).
 //-1: -1, 0: number, 1: +, 2: -, 3: *, 4: /, 5: ^.
-void put(int seed[length_], int pos, int numCounter, int symCounter) {
+void put(vector<int> v, int pos, int numCounter, int symCounter, int length_) {
+  //cout << "put called" << endl;
   if (pos == length_) {
-    if (checker(seed)) {
+    if (checker(v)) {
       counter++;
-      print(seed);
+      print(v);
     }
+    calculation++;
     return;
   }
   int lower = 0;
   int upper = 6;
-  if (numCounter == 6) {
+  if (numCounter == (length_ + 1) / 2) {
     lower = 1;
   }
-  if (symCounter == 5 || symCounter == numCounter - 1) {
+  if (symCounter == (length_ + 1) / 2 - 1 || symCounter == numCounter - 1) {
     upper = 1;
   }
   while (lower < upper) {
-    seed[pos] = lower;
-    if (lower == 0)
-      put(seed, pos + 1, numCounter + 1, symCounter);
+    if (pos == (int) v.size())
+      v.push_back(lower);
+    else if (pos > (int) v.size())
+      cout << "something went wrong" << endl;
     else
-      put(seed, pos + 1, numCounter, symCounter + 1);
+      v[pos] = lower;
+    if (lower == 0)
+      put(v, pos + 1, numCounter + 1, symCounter, length_);
+    else
+      put(v, pos + 1, numCounter, symCounter + 1, length_);
     lower++;
 
   }
@@ -105,36 +249,23 @@ void put(int seed[length_], int pos, int numCounter, int symCounter) {
 }
 
 //check if the RPN stored in the array gives the desired result.
-bool checker(int seed[length_]) {
+bool checker(vector<int> seed) {
 
-  double a = 1.0, b = 1.0, c = 4.0, d = 5.0, e = 1.0, f = 4.0;
+  // double a = 1.0, b = 1.0, c = 4.0, d = 5.0, e = 1.0, f = 4.0;
+  //cout << "checker called" << endl;
   stack<double> myStack;
   int sign;
   double firstNum, secondNum;
   int numCount = 0;
-  for (int i = 0; i < length_; i++) {
-    if (seed[i] == 0) {
-      switch (numCount) {
-        case 0: myStack.push(a);
-                break;
-        case 1: myStack.push(b);
-                break;
-        case 2: myStack.push(c);
-                break;
-        case 3: myStack.push(d);
-                break;
-        case 4: myStack.push(e);
-                break;
-        case 5: myStack.push(f);
-                break;
-      }
+  for (int i : seed) {
+    if (i == 0) {
+      myStack.push(base[numCount]);
       numCount++;
-
-    } else if (seed[i] == -1) {
-      myStack.push(-a);
+    } else if (i == -1) {
+      myStack.push(-1 * base[0]);
       numCount++;
     } else {
-        sign = seed[i];
+        sign = i;
         secondNum = myStack.top();
         myStack.pop();
         firstNum = myStack.top();
@@ -153,6 +284,7 @@ bool checker(int seed[length_]) {
         }
       }
     }
+  // cout << "checker done" << endl;
   if (myStack.top() == desired) {
     return true;
   } else {
@@ -162,6 +294,8 @@ bool checker(int seed[length_]) {
 }
 
 string translate(string input) {
+// cout << "translate called" << endl;
+
   string symbol = "+-*/^";
 
   stack<string> output;
@@ -169,19 +303,28 @@ string translate(string input) {
   string str2;
   string outstr = "";
   for (size_t i = 0; i < input.size(); i++) {
+    if (input.at(i) == ' ') {
+      continue;
+    }
     if ((int)symbol.find(input.at(i)) == -1) {
-      output.push(string(1, input.at(i)));
-    } else {
-      if (i == 1) {
-        output.pop();
-        output.push("-1");
-        continue;
+      string total = "";
+      while (input.at(i) != ' ') {
+        if (i == 1) {
+          if (input.at(i) == '-') {
+            total = "-1";
+            break;
+          }
+        }
+        total += string(1, input.at(i));
+        i++;
       }
+      output.push(total);
+    } else {
       str2 = output.top();
       output.pop();
       str1 = output.top();
       output.pop();
-      if (i == input.size() - 1) {
+      if (i == input.size() - 2) {
         output.push(str1 + string(1, input.at(i)) + str2);
       } else {
         output.push("(" + str1 + string(1, input.at(i)) + str2 + ")");
@@ -192,43 +335,34 @@ string translate(string input) {
 }
 
 //print RPN.
-void print(int seed[length_]) {
+void print(vector<int> seed) {
+  // cout << "print called" << endl;
+
   int numCount = 0;
-  int i;
+  // int i;
   string output = "";
-  for (int k = 0; k < length_; k++) {
-    i = seed[k];
+  for (int i : seed) {
     switch (i) {
-      case -1: output += "1-";
+      case -1: output += to_string((int) base[0]);
+               output += "- ";
                numCount++;
                break;
-      case 0: switch (numCount) {
-               case 0: output += "1";
-                       break;
-               case 1: output += "1";
-                       break;
-               case 2: output += "4";
-                       break;
-               case 3: output += "5";
-                       break;
-               case 4: output += "1";
-                       break;
-               case 5: output += "4";
-                       break;
-              }
+      case 0: output += to_string((int) base[numCount]);
+              output += " ";
               numCount++;
               break;
-      case 1: output += "+";
+      case 1: output += "+ ";
               break;
-      case 2: output += "-";
+      case 2: output += "- ";
               break;
-      case 3: output += "*";
+      case 3: output += "* ";
               break;
-      case 4: output += "/";
+      case 4: output += "/ ";
               break;
-      case 5: output += "^";
+      case 5: output += "^ ";
     }
   }
   //cout << output << endl;
+  // cout << std::to_string(desired) + "=" + output << endl;
   cout << std::to_string(desired) + "=" + translate(output) << endl;
 }
--- main.h
+++ main.h
@@ -1,8 +1,27 @@
 #pragma once
+#include <iostream>
 #include <string>
+#include <stack>
+#include <array>
+#include <queue>
+#include <cmath>
+#include <vector>
+using std::queue;
+using std::vector;
+using std::pow;
+using std::cout;
+using std::cin;
+using std::endl;
 using std::string;
+using std::to_string;
+using std::stack;
+using std::stoi;
 
-void put(int * seed, int pos, int numCounter, int symCounter);
-bool checker(int * seed);
-void print(int * seed);
+void put(vector<int> v, int pos, int numCounter, int symCounter, int length_);
+void put(vector<int> v, int max_length);
+bool checker(vector<int> seed);
+void print(vector<int> seed);
 string translate(string input);
+void choose(queue<char> remain, vector<double> current);
+void strongProof();
+void weakProof();
